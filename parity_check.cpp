00e729d72599c2b19385ad9c4e2e60c0c2f5e64c
SK <srikanthkn10@gmail.cm>
2025-12-14 23:45:41 +0530

    Day 2

---
+++ parity_check.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+  int num;
+  std::cout << "Enter the number: ";
+  std::cin >> num;
+
+  if (num == 0)
+  {
+    std::cout << "Neither";
+  }
+  else if (num % 2 == 0)
+  {
+    std::cout << "Number is even";
+  }
+  else
+  {
+    std::cout << "Number is odd";
+  }
+  return 0;
+}
