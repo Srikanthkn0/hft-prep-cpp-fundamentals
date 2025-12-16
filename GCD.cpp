543db62359f614b26fbdedf0af161807612f0456
SK <srikanthkn10@gmail.com>
2025-12-16 11:14:10 +0530

    Refactored GCD program by removing string concatenation code and implementing GCD calculation

--- GCD.cpp
+++ GCD.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <string>
 
 int main(){
-    std::string str,str1,str3;
-    std::cout<<"Enter the first string: ";
-    std::cin>>str;
-    std::cout<<"Enter the second string: ";
-    std::cin>>str1;
-    str3=str1+str;
-    std::cout<<"The concatenated string is: "<<str3<<std::endl;
-    std::cout<<str3.length()<<std::endl;
-    std::cout<<str3.size()<<std::endl;
-    std::cout<<str3[3]<<std::endl;
+    int a,b;
+    std::cout<<"Enter two numbers: ";
+    std::cin>>a>>b;
+    while(b!=0){
+        int remainder = a % b;
+        a=b;
+        b=remainder;
+    }
+    std::cout<< "GCD is: "<<a<<std::endl;
     return 0;
+
 }
